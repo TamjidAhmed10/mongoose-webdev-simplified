bc50a0806fe81dd4965530ad10a5fb23ba3cead4
tamjid10 <tamjidahammad10@gmail.com>
2022-03-23 06:40:53 +0600

    new schema learn

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hi mom");
+const mongoose = require("mongoose");
+const User = require("./Users");
+mongoose.connect("mongodb://localhost:27017/test", () => {
+  console.log("connected");
+});
+const run = async () => {
+  const user = await User.create({ name: "Tamjid", age: 23 });
+  console.log(user);
+};
+run();
