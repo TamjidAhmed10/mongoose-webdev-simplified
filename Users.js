c7eac9b94ccbecc19cd64e16ec690bf89e289ff6
tamjid10 <tamjidahammad10@gmail.com>
2022-03-24 06:53:21 +0600

    express install

--- Users.js
+++ Users.js
@@ -1,8 +1,39 @@
 const mongoose = require("mongoose");
-
+const addressSchema = new mongoose.Schema({
+  city: String,
+  country: String,
+});
 const userSchema = new mongoose.Schema({
   name: String,
-  age: Number,
+  age: {
+    type: Number,
+    min: 1,
+    max: 100,
+    validate: {
+      validator: (v) => {
+        return v % 2 === 0;
+      },
+      message: (props) => {
+        return `${props.value} is not a even Number`;
+      },
+    },
+  },
+  email: { type: String, required: true, lowercase: true },
+  createdAt: {
+    type: Date,
+    default: () => {
+      return Date.now();
+    },
+  },
+  updatedAt: {
+    type: Date,
+    default: () => {
+      return Date.now();
+    },
+  },
+  bestFriend: mongoose.SchemaTypes.ObjectId,
+  address: addressSchema,
+  hobbies: [String],
 });
 
 module.exports = mongoose.model("User", userSchema);
--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const mongoose = require("mongoose");
 const User = require("./Users");
-mongoose.connect("mongodb://localhost:27017/test", () => {
-  console.log("connected");
-});
+mongoose.connect("mongodb://localhost:27017/test");
 const run = async () => {
-  const user = await User.create({ name: "Tamjid", age: 23 });
-  console.log(user);
+  try {
+    const user = await User.create({
+      name: "Tamjid Ahmed",
+      age: 99,
+      address: { city: "Dhaka", country: "BD" },
+      hobbies: ["writing", "eating", "kennel"],
+      email: "Tamjid@gmail.com",
+    });
+    console.log(user);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 run();
