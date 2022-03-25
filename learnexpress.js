90d54d528a7c441fafc243fd694a642f5a45ce49
tamjid10 <tamjidahammad10@gmail.com>
2022-03-25 12:42:36 +0600

    done

--- learnexpress.js
+++ learnexpress.js
@@ -1,1 +1,6 @@
-console.log("Started");
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.status(400).json({ message: "Error" });
+});
+app.listen(3000);
