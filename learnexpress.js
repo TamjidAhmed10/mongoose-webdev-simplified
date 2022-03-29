02430b646434a6c01fdc5497e5bbd3c2998ee7e0
tamjid10 <tamjidahammad10@gmail.com>
2022-03-30 00:39:26 +0600

    dd

--- learnexpress.js
+++ learnexpress.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => {
-  res.status(400).json({ message: "Error" });
-});
+
+app.get("/", (req , res) => {
+  res.status(201).send("hello world")
+})
+
+
+// app.get("/", (req, res) => {
+//   res.status(200).json({ message: " no Error" });
+// });
+// app.get("/about", (req, res) => { 
+//   res.status(200).send("About page");
+// })
 app.listen(3000);
